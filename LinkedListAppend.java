4a0925f6b78f911dec452f128bf34aff58545522
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-08-03 01:28:24 +0700

    append specified element to end of a linked list

---
+++ LinkedListAppend.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+import java.util.LinkedList;
+public class LinkedListAppend {
+  public static void main(String[] args) {
+    
+     LinkedList<String> list = new LinkedList<String>();
+  
+          list.add("Red");
+          list.add("Green");
+          list.add("Black");
+          list.add("White");
+          list.add("Pink");
+          list.add("Yellow");
+   System.out.println("The linked list: " + list);
+   }
+}

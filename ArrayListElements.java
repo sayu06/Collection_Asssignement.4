bf796f2ace5aa20e19224b72950711e059e3091d
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-08-03 00:41:42 +0700

    array list, add some colors  and print out them.

---
+++ ArrayListElements.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+  import java.util.ArrayList;
+public class ArrayListElements {
+   public static void main(String [] args) {
+        ArrayList<String> color = new ArrayList<String>();
+        color.add("Green");
+        color.add("Yellow");
+        color.add("Black");
+        color.add("Red");
+        System.out.print(color);
+    }
+}
+

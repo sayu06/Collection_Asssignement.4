964af5052a4a006482d4c23581662b24da6dc99f
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-08-04 12:37:21 +0700

    append specified element to the end of a hash set.

---
+++ HashSetAppendElements.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+    import java.util.HashSet;
+public class HashSetAppendElements {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<String>();
+          set.add("Red");
+          set.add("Green");
+          set.add("Black");
+          set.add("White");
+          set.add("Pink");
+          set.add("Yellow");
+          System.out.println("The Hash Set: " + set);
+   }
+}
+
